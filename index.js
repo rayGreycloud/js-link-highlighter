876f8070d66a8ce75d2cd262caf6408d32343c04
rayGreycloud <rayGreycloud@gmail.com>
2016-12-29 07:07:03 -0800

    Refactor to adjust for window scroll

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ document.body.append(highlight);
 function highlightLink() {
   // Get coords of link
   const linkCoords = this.getBoundingClientRect();
+  // Adjust coords for scroll
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+  };
+
   // Set highlight width/height
-  highlight.style.width = `${linkCoords.width}px`;
-  highlight.style.height = `${linkCoords.height}px`;
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
   // Set position with transform
-  highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
 // Listen for mouse over
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
